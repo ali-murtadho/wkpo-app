0a659032889c48042663e64bdd0f8ef5f43c48c9
alimur16 <murtadhoali22@gmail.com>
2024-06-19 17:24:15 +0700

    update before push notif and url middleware

--- model/admin.py
+++ model/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# readcsv/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import ClassificationResult
+
+admin.site.register(ClassificationResult)
